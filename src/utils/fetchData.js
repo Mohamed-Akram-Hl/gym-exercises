ba68c7305c5b09b56048130eddaa8ea7834b4e5c
Akram <akramhelali007@outlook.com>
2022-06-25 09:34:13 +0100

    Api change

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '140d2c151amshccdd08ec4457238p122e87jsn7bf050d0021c',
+    'X-RapidAPI-Key': 'a7b60916d1msha9a87796315c054p1aa1bejsn30e83fa7db47',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
