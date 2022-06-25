a32c82aff357a8c45f6bf499c1e65f970d527e49
Akram <akramhelali007@outlook.com>
2022-06-25 09:36:35 +0100

    youtube

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '140d2c151amshccdd08ec4457238p122e87jsn7bf050d0021c',
+    'X-RapidAPI-Key': 'a7b60916d1msha9a87796315c054p1aa1bejsn30e83fa7db47',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
